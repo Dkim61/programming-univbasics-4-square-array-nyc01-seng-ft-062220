7004788f10e38ac0c3d26ed4fcae3e1afb00035f
david <dkim187@binghamton.edu>
2020-06-17 03:02:06 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,8 @@
-def square_array(array)
-  # your code here
+def square_array(array) 
+  new_squared_array = []
+    array.each { |number|
+      squared_number = number ** 2
+      array << squared_number
+    }
+  return new_squared_array
 end
