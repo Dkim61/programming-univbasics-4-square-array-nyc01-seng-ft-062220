f8077a69cbfaadf4ff6db46672a9963cb518bf36
david <dkim187@binghamton.edu>
2020-06-17 03:17:48 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,8 +1,10 @@
 def square_array(array) 
+  count = 0
   new_squared_array = []
-    array.each { |number|
+    while count < array.length do
       squared_number = number ** 2
       array << squared_number
-    }
+      count += 1
+    end
   return new_squared_array
 end
